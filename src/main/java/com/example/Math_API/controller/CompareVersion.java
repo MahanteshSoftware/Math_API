7bbc6c21d16f704f6335aab1b00b6672ab2de739
Mahantesh Nishandar <mahanteshnishandar@Mahanteshs-MacBook-Air.local>
2023-06-26 21:39:23 +0100

    intial commint to git hub

--- src/main/java/com/example/Math_API/controller/CompareVersion.java
+++ src/main/java/com/example/Math_API/controller/CompareVersion.java
@@ -1,2 +1,55 @@
-package src.main.java.com.example.Math_API.controller;public class CompareVersion {
+package src.main.java.com.example.Math_API.controller;
+
+public class CompareVersion {
+
+    public static void main(String[] args) {
+        String version1 = "1.3.4";
+        String version2 = "1.10";
+
+        int result = compareVersion(version1, version2);
+
+        if (result < 0)
+            System.out.println("Result is : "+result+", "+version1 + " < " + version2);
+        else if (result > 0)
+            System.out.println("Result is : "+result+", "+version1 + " > " + version2);
+        else
+            System.out.println("Result is : "+result+", "+version1 + " = " + version2);
+    }
+
+    public static int compareVersion(String version1, String version2) {
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        int i=0;
+        while(i<arr1.length || i<arr2.length)
+        {
+            if(i<arr1.length && i<arr2.length)
+            {
+                if(Integer.parseInt(arr1[i]) < Integer.parseInt(arr2[i]))
+                {
+                    return -1;
+                }
+                else if(Integer.parseInt(arr1[i]) > Integer.parseInt(arr2[i]))
+                {
+                    return 1;
+                }
+            }
+            else if(i<arr1.length)
+            {
+                if(Integer.parseInt(arr1[i]) != 0)
+                {
+                    return 1;
+                }
+            }
+            else if(i<arr2.length)
+            {
+                if(Integer.parseInt(arr2[i]) != 0)
+                {
+                    return -1;
+                }
+            }
+            i++;
+        }
+        return 0;
+    }
+
 }
--- src/main/java/com/example/Math_API/controller/MathAPIController.java
+++ src/main/java/com/example/Math_API/controller/MathAPIController.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class MathAPIController {
+package src.main.java.com.example.Math_API.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/mathapi")
+public class MathAPIController {
+
+    @GetMapping("/min")
+    public List<Double> getMin(@RequestParam List<Double> numbers, @RequestParam int quantifier) {
+        List<Double> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers.subList(0, Math.min(quantifier, sortedNumbers.size()));
+    }
+
+    @GetMapping("/max")
+    public List<Double> getMax(@RequestParam List<Double> numbers, @RequestParam int quantifier) {
+        List<Double> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers, Collections.reverseOrder());
+        return sortedNumbers.subList(0, Math.min(quantifier, sortedNumbers.size()));
+    }
+
+    @GetMapping("/avg")
+    public double getAverage(@RequestParam List<Double> numbers) {
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.size();
+    }
+
+    @GetMapping("/median")
+    public double getMedian(@RequestParam List<Double> numbers) {
+        List<Double> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        int size = sortedNumbers.size();
+        if (size % 2 == 0) {
+            int midIndex = size / 2;
+            return (sortedNumbers.get(midIndex - 1) + sortedNumbers.get(midIndex)) / 2.0;
+        } else {
+            return sortedNumbers.get(size / 2);
+        }
+    }
+
+    @GetMapping("/percentile")
+    public double getPercentile(@RequestParam List<Double> numbers, @RequestParam double q) {
+        List<Double> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        int n = sortedNumbers.size();
+        int index = (int) Math.ceil(q * n / 100) - 1;
+        return sortedNumbers.get(index);
+    }
 }
